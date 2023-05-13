78c97da2e4f211a7e0e02b551d1f79158e45a491
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-05-13 14:43:44 +0300

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Yasin Erdemli on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
